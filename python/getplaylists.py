b5e5b44bff0839ae9bd65c22c4eb315f108640a5
Emily Tang <emitang35@gmail.com>
2025-07-30 19:04:51 -0400

    getplaylists method using new token fxn

---
+++ python/getplaylists.py
@@ -0,0 +1,34 @@
+import requests
+import json
+import secrets
+import gettoken
+import pandas as pd
+
+token = gettoken.get_token()
+
+headers = {'Content-type': 'application/json', 'Authorization': token['token_type'] + ' ' + token['access_token']}
+
+url = 'https://api.spotify.com/v1/users/' + secrets.username + '/playlists'
+response = requests.get(url, headers=headers)
+
+# Check if the response is successful
+if response.status_code == 200:
+    data = response.json()['items']
+else:
+    print("error!")
+
+print(data)
+
+# let token = await getToken();
+    # // console.log(token.token_type + ' ' + token.access_token)
+    # const response = await fetch('https://api.spotify.com/v1/users/' + secret.username + '/playlists', {
+    #   method: 'GET',
+    #   headers: {
+    #     'Content-Type': 'application/json',
+    #     'Authorization': token.token_type + ' ' + token.access_token
+    #   },
+    #   //body: body
+    # });
+    # const results = await response.json();
+    # //console.log(data);
+    # return results.items;
--- python/gettoken.py
+++ python/gettoken.py
@@ -2,17 +2,21 @@ import requests
 import json
 import secrets
 
-headers = {'Content-type': 'application/x-www-form-urlencoded'}
+def get_token(): 
+    headers = {'Content-type': 'application/x-www-form-urlencoded'}
 
-data = 'grant_type=client_credentials&client_id=' + secrets.client_id + '&client_secret=' + secrets.client_secret
+    data = 'grant_type=client_credentials&client_id=' + secrets.client_id + '&client_secret=' + secrets.client_secret
 
-url = "https://accounts.spotify.com/api/token"
-response = requests.post(url, headers=headers, data=data)
+    url = "https://accounts.spotify.com/api/token"
+    response = requests.post(url, headers=headers, data=data)
 
-# Check if the response is successful
-if response.status_code == 200:
-    data = response.json()
-else:
-    print("error!")
+    # Check if the response is successful
+    if response.status_code == 200:
+        token = response.json()
+    else:
+        print("error!")
 
-print(data)
+    print(token)
+    return token
+
+get_token()
